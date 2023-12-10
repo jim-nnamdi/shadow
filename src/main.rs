0521eb5a48b41b7063aa9b1ca8c43da123ac3400
jim-nnamdi <jimsamuel50@gmail.com>
2023-12-10 13:13:32 +0100

    List Base Shadow Algorithms For SmokeTest & Usage Before Segregation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
+use rand::rngs::OsRng;
+use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
+use secp256k1::{PublicKey, SecretKey};
 
-mod singlewalk;
-mod randomwalk;
+use anyhow::{Ok, Result};
 mod euclidean;
+mod randomwalk;
+mod singlewalk;
 
-
-fn main() {
-    println!("Hello, world!");
+pub fn shadow_tx_id_hash() -> Result<(SecretKey, PublicKey)> {
+    let secp = secp256k1::Secp256k1::new();
+    let (sk, pk) = secp.generate_keypair(&mut OsRng);
+    Ok((sk, pk))
 }
+
+pub fn shadow_block_id_hash() -> Result<(SecretKey, PublicKey)> {
+    let secp = secp256k1::Secp256k1::new();
+    let (sk, pk) = secp.generate_keypair(&mut OsRng);
+    Ok((sk, pk))
+}
+
+pub fn shadow_encrypt_block_data(bits_size: usize, block_data: &[u8]) -> Result<Vec<u8>> {
+    let mut rng = rand::thread_rng();
+    let shadow_private = RsaPrivateKey::new(&mut rng, bits_size)?;
+    let shadow_public = RsaPublicKey::from(&shadow_private);
+    let shadow_data_encrypt = shadow_public.encrypt(&mut rng, Pkcs1v15Encrypt, block_data)?;
+    Ok(shadow_data_encrypt)
+}
+
+pub fn shadow_decrypt_block_data(bits_size: usize, block_data: Vec<u8>) -> Result<Vec<u8>> {
+    let mut rng = rand::thread_rng();
+    let shadow_private = RsaPrivateKey::new(&mut rng, bits_size)?;
+    let shadow_data_decrypt = shadow_private.decrypt(Pkcs1v15Encrypt, &block_data)?;
+    Ok(shadow_data_decrypt)
+}
+
+fn main() {}
